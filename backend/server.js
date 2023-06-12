00840752cc0d9343203abc30421611d74bcdfd47
Andres Gonzalez <a_gonzalez214@hotmail.com>
2023-06-11 23:30:02 -0300

    added code to work on render

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
+
 dotenv.config();
 
 mongoose
